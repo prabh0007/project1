860ab7ab8c4537c8394eeede82fada16e0b560ee
prabh0007 <prabhnoor37@gmail.com>
2023-11-24 11:13:29 -0500

    first commit

---
+++ controllers/getPost.js
@@ -0,0 +1,5 @@
+const BlogPost = require('../models/BlogPost')
+module.exports = async(req, res) => {
+    const blogpost  = await BlogPost.findById(req.params.id).populate('userid')
+    res.render('post', {blogpost});
+}
---
+++ controllers/storeUser.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+const path = require('path')
+module.exports = async(req, res) => {
+    try{
+        const user  = await User.create(req.body)
+        res.redirect("/")
+    }
+    catch(error){
+        if (error.name === 'ValidationError') {
+            const validationErrors = Object.keys(error.errors).map(key =>
+            error.errors[key].message);
+            console.log(validationErrors)
+            // req.session.validationErrors = validationErrors;
+            req.flash('validationErrors', validationErrors);
+            return res.redirect('/auth/register');
+            }
+            console.error(error);
+            res.status(500).send('An error occurred while creating the user');
+            }
+                  
+        // console.log(error)
+        // res.status(500).send('error occured while creating user')
+        
+}
